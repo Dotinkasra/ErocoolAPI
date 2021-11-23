e9d43a49f78ea57e993254aa801b6d83a57dda81
Dotinkasra <tyekepon@pm.me>
2021-11-23 16:19:24 +0900

    fix : fieldで型指定を行うように修正

--- ErocoolAPI/schemas/data.py
+++ ErocoolAPI/schemas/data.py
@@ -4,16 +4,16 @@ from dataclasses import field
 @dataclass
 class Data():
     _ja_title: str = str(random.randrange(10**6, 10**7))
-    _en_title: str = ''
-    _upload_date: str = ''
-    _artists: list[str] = field(default_factory=list)
-    _lang: str = ''
+    _en_title: str = field(default_factory=str)
+    _upload_date: str = field(default_factory=str)
+    _artists: list[str] = field(default_factory=list[str])
+    _lang: str = field(default_factory=str)
     _groups: list[str] = field(default_factory=list[str])
     _parodies: list[str] = field(default_factory=list[str])
     _tags: list[str] = field(default_factory=list[str])
-    _total_pages: int = 0
-    _thumbnail: str = ''
-    _url: str = ''
+    _total_pages: int = field(default_factory=int)
+    _thumbnail: str = field(default_factory=str)
+    _url: str = field(default_factory=str)
     _image_list: list[str] = field(default_factory=list[str])
     
     @property
@@ -21,7 +21,7 @@ class Data():
         return self._ja_title
 
     @ja_title.setter
-    def ja_title(self, value):
+    def ja_title(self, value: str):
         if value == None or value == '':
             return
         self._ja_title = value
@@ -31,7 +31,7 @@ class Data():
         return self._en_title
 
     @en_title.setter
-    def en_title(self, value):
+    def en_title(self, value: str):
         self._en_title = value
 
     @property
@@ -39,7 +39,7 @@ class Data():
         return self._upload_date
 
     @upload_date.setter
-    def upload_date(self, value):
+    def upload_date(self, value: str):
         self._upload_date = value
 
     @property
@@ -47,7 +47,7 @@ class Data():
         return self._artists
 
     @artists.setter
-    def artists(self, value):
+    def artists(self, value: list[str]):
         self._artists = value
 
     @property
@@ -55,7 +55,7 @@ class Data():
         return self._lang
 
     @lang.setter
-    def lang(self, value):
+    def lang(self, value: str):
         self._lang = value
 
     @property
@@ -63,7 +63,7 @@ class Data():
         return self._groups
 
     @groups.setter
-    def groups(self, value):
+    def groups(self, value: list[str]):
         self._groups = value
 
     @property
@@ -71,7 +71,7 @@ class Data():
         return self._parodies
 
     @parodies.setter
-    def parodies(self, value):
+    def parodies(self, value: list[str]):
         self._parodies = value
 
     @property
@@ -79,7 +79,7 @@ class Data():
         return self._tags
 
     @tags.setter
-    def tags(self, value):
+    def tags(self, value: list[str]):
         self._tags = value
 
     @property
@@ -87,7 +87,7 @@ class Data():
         return self._total_pages
 
     @total_pages.setter
-    def total_pages(self, value) -> int:
+    def total_pages(self, value: int):
         if type(value) is not int:
             self._total_pages = int(value)
         self._total_pages = value
@@ -97,7 +97,7 @@ class Data():
         return self._thumbnail
 
     @thumbnail.setter
-    def thumbnail(self, value):
+    def thumbnail(self, value: str):
         self._thumbnail = value
     
     @property
@@ -105,7 +105,7 @@ class Data():
         return self._url
     
     @url.setter
-    def url(self, url):
+    def url(self, url: str):
         self._url = url
 
     @property
@@ -113,7 +113,7 @@ class Data():
         return self._image_list
 
     @image_list.setter
-    def image_list(self, value):
+    def image_list(self, value: list[str]):
         if value == None:
             self._image_list = []
         self._image_list = value
